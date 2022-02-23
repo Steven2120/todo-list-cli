d6b0f591c53c14ec734c8fee689af97b532a1a7a
Steven Moyano <stevenmoyano1@gmail.com>
2022-02-22 18:17:53 -0600

    added notification to user if there are no todos or if they enter the wrond entry

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const ps = require("prompt-sync");
 const prompt = ps();
-// let complete = prompt("Complete a to-do item by entering 2: ");
 
 let todos = [];
 let areTodosComplete = [false, true];
@@ -9,16 +8,15 @@ let incomplete = "incomplete";
 let counter = 0;
 let placeHolder = [];
 
-// console.log("[1] Create a to-do item");
-// console.log("[2] Complete a to-do item");
-
-// let createOrComplete = Number(prompt());
-
-while (counter < 100) {
+while (counter < Infinity) {
   console.log("[1] Create a to-do item");
   console.log("[2] Complete a to-do item");
 
   let createOrComplete = Number(prompt());
+  if (createOrComplete !== 1 || createOrComplete !== 2) {
+    console.log("Wrong entry. enter 1 or 2");
+  }
+
   if (createOrComplete === 1) {
     let createdName = prompt("What is this to-do item called?: ");
     if (createdName === "done") {
@@ -30,6 +28,8 @@ while (counter < 100) {
         console.log(i + 1 + "." + " " + incomplete + " " + todos[i]);
       }
     }
+  } else if (createOrComplete === 2 && todos.length === 0) {
+    console.log("You have no todos!");
   } else if (createOrComplete === 2) {
     let completeTodo = Number(
       prompt("Which to-do item would you like to complete?: ")
